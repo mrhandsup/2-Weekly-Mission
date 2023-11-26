f6455eb4ebde3ecf02ad717cbb7a0b7ffd10fa33
Ohreum <dhfma3394@naver.com>
2023-11-26 16:01:54 +0900

    style: 카드 이미지 데이터 없을 때 기본 이미지로 설정 및 검색 디자인 추가

--- src/App.js
+++ src/App.js
@@ -5,11 +5,17 @@ import FolderHeader from './components/FolderHeader';
 import FolderCard from './components/FolderCard';
 import { getFolder, getProfile } from './api';
 import { useEffect, useState } from 'react';
+import searchImg from './assets/search.png';
 
 function App() {
   const [profile, setProfile] = useState({});
   const [folder, setFolder] = useState({});
+  const [search, setSearch] = useState('');
 
+  const handleSearchSubmit = e => {
+    e.preventDefault();
+    setSearch(e.target['search'].value);
+  };
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -29,7 +35,15 @@ function App() {
     <>
       <Nav profile={profile} />
       <FolderHeader folder={folder} />
-      <FolderCard links={folder.links} />
+      <div className="folder-body">
+        <form onSubmit={handleSearchSubmit}>
+          <div className="search-area">
+            <img src={searchImg} alt="검색" />
+            <input name="search" className="link-search" placeholder="링크를 검색해보세요" />
+          </div>
+        </form>
+        <FolderCard links={folder.links} />
+      </div>
       <Footer />
     </>
   );
--- src/components/FolderCard.js
+++ src/components/FolderCard.js
@@ -1,3 +1,5 @@
+import noImage from '../assets/no-image.png';
+
 function formatDate(value) {
   const date = new Date(value);
   return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
@@ -9,19 +11,17 @@ function FolderCard({ links }) {
   }
 
   return (
-    <div className="folder-body">
-      <ul className="folder-card">
-        {links.map(link => (
-          <li className="link-card" key={link.id}>
-            <img src={link.imageSource} alt="카드 이미지" />
-            <div className="info-area">
-              <p className="title">{link.title}</p>
-              <span className="date">{formatDate(link.createdAt)}</span>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <ul className="folder-card">
+      {links.map(link => (
+        <li className="link-card" key={link.id}>
+          <img src={link.imageSource || noImage} alt="카드 이미지" />
+          <div className="info-area">
+            <p className="title">{link.title}</p>
+            <span className="date">{formatDate(link.createdAt)}</span>
+          </div>
+        </li>
+      ))}
+    </ul>
   );
 }
 
